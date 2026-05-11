0653097360fc5741bfd9345f625c1c7d32533098
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-05-10 19:24:56 -0700

    Complete foundations 6-11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let j = 0
+    while (j < arr.length) {
+      if (arr[j] === nums[i]) {
+        arr.splice(j, 1)
+      } else {
+        j++
+      }
+    }
+  }
+  return arr
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a <= 0 || b <= 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR"
+  }
+  if (a > b) {
+    [a, b] = [b, a]
+  }
+  let sum = 0
+  for (let i = a; i <= b; i++) {
+    sum += i
+  }
+  return sum
 };
+
+sumAll(1, 3)
 
 // Do not edit below this line
 module.exports = sumAll;

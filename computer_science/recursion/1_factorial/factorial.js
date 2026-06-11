6c407ad6ec4ece7f1991a606f7ea075545eeae4d
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-06-10 22:59:21 -0700

    complete factorial solution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (n < 0 || !Number.isInteger(n)) {
+    return;
+  }
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line

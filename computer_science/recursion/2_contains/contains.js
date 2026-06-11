c9b8555df0d069911f427820a758cab5b5474318
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-06-10 23:21:46 -0700

    complete 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,19 @@
-const contains = function() {
-  
+const contains = function (obj, val) {
+  let res = false;
+  for (const prop in obj) {
+    if (obj[prop] === val) {
+      return true;
+    } else if (Number.isNaN(obj[prop]) && Number.isNaN(val)) {
+      return true;
+    } else if (obj[prop] !== null && typeof obj[prop] === "object") {
+      res = contains(obj[prop], val);
+      if (res) {
+        return true;
+      }
+    }
+  }
+  return res;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;

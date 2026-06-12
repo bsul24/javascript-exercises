7d34b2789f71672b28f0a9a3fa315a60c8cc47ee
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-06-11 23:29:44 -0700

    Complete 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,21 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length <= 1) {
+    return [arr];
+  }
+
+  const results = [];
+  const lastVal = arr.at(-1);
+  const prev = permutations(arr.slice(0, -1));
+
+  for (const el of prev) {
+    for (let i = 0; i < arr.length; i++) {
+      const temp = [...el];
+      temp.splice(i, 0, lastVal);
+      results.push(temp);
+    }
+  }
+  return results;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;

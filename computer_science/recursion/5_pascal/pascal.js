89be761b404fe530bf7171bc6f4f07a833191282
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-06-12 00:00:38 -0700

    Complete 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,18 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+  const prev = pascal(n - 1);
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || i === prev.length) {
+      result.push(1);
+    } else {
+      result.push(prev[i] + prev[i - 1]);
+    }
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
